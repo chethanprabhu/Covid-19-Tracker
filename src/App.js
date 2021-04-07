23844805a82f48127fa6781ce78c2c79478456ef
Chethan Prabhu <chethanprabhu65@gmail.com>
2021-04-07 23:17:31 +0530

    added basic structure and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import Cards from "./components/Cards/Cards"
+import Chart from "./components/Chart/Chart"
+import CountryPicker from "./components/CountryPicker/CountryPicker"
+
+import styles from "./App.module.css"
 const App = () => {
     return (
-        <h1>Covid 19 Tracker</h1>
+        <div className={styles.container}>
+            <Cards />
+            <CountryPicker />
+            <Chart />
+        </div>
     )
 }
 
