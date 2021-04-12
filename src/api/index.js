eda4f7488a020c74effd534c70cc7b3d18b46401
Chethan Prabhu <chethanprabhu65@gmail.com>
2021-04-12 23:50:24 +0530

    added index changes

--- src/api/index.js
+++ src/api/index.js
@@ -8,11 +8,25 @@ const api = axios.create({
 
 export const getWorldData = async () => {
  try {
-    const {data: {cases, active, deaths, updated}} = await api.get("v3/covid-19/all");
+    const {data: {cases, recovered, deaths, updated}} = await api.get("v3/covid-19/all");
     const lastUpdated = new Date(updated).toString().substr(4, 20);
-    return {cases, active, deaths, lastUpdated};
+    return {cases, recovered, deaths, lastUpdated};
  }
  catch(err) {
     console.log(err);
  }
 }
+
+export const getWorldDataYesterday = async () => {
+   try {
+      const {data: {cases, recovered, deaths}} = await api.get("v3/covid-19/all", {
+         params: {
+            yesterday: true
+         }
+      });
+      return {cases, recovered, deaths};
+   }
+   catch(err) {
+      console.log(err);
+   }
+  }
