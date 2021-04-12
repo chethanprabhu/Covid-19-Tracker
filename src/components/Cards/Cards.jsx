52fd57e9bf7c62c04a5624cb76ae22808aefe742
Chethan Prabhu <chethanprabhu65@gmail.com>
2021-04-12 23:50:51 +0530

    added cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Card from "./Card/Card"
 
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props);
     return (
-        <h1>Cards</h1>
+        <>
+            <Card count={props.state.totalCases} todayCount={props.state.newCases} cardName="Total Cases" color="yellow"/>
+            <Card count={props.state.totalRecovered} todayCount={props.state.newRecovered} cardName="Total Recovered" color="green"/>
+            <Card count={props.state.totalDeaths} todayCount={props.state.newDeaths} cardName="Total Deaths" color="red"/>
+        </>
     )
 }
 
