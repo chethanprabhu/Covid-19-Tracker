7f2bfc4da59c52fbad0cc83200931b20917ff691
Chethan Prabhu <chethanprabhu65@gmail.com>
2021-04-12 23:50:30 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getWorldData } from "./api"
+import { getWorldData, getWorldDataYesterday } from "./api"
 
 import Cards from "./components/Cards/Cards"
 import Chart from "./components/Chart/Chart"
@@ -8,16 +8,35 @@ import CountryPicker from "./components/CountryPicker/CountryPicker"
 import styles from "./App.module.css"
 class App extends React.Component {
 
+    state = {
+        totalCases: 0,
+        totalRecovered: 0,
+        totalDeaths: 0,
+        newCases: 0,
+        newRecovered: 0,
+        newDeaths: 0
+    }
+
     async componentDidMount()  {
         const data = await getWorldData();
+        const dataY = await getWorldDataYesterday();
+    
+        this.setState({
+            totalCases: data.cases,
+            totalRecovered: data.recovered,
+            totalDeaths: data.deaths,
+            newCases: data.cases - dataY.cases,
+            newRecovered: data.recovered - dataY.recovered,
+            newDeaths: data.deaths - dataY.deaths
+        })
     }
 
     render() {
         return (
             <div className={styles.container}>
-                <Cards />
-                <CountryPicker />
-                <Chart />
+                <Cards state={this.state}/>
+                {/* <CountryPicker />
+                <Chart /> */}
             </div>
         )
     }
